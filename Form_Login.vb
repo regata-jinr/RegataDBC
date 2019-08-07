a384910036f0761e096ad5a733fe7a56776174cd
Boris Rumyantsev <bdrum@jinr.ru>
2019-08-07 17:40:15 +0300

    Keep sign in feature;
    Some Reorganisations of git

---
+++ Form_Login.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Data.SqlClient
+Public Class Form_Login
+
+
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        Try
+
+            If CheckBoxKeep.Checked Then
+                My.Settings.user = UsernameTextBox.Text
+                My.Settings.password = PasswordTextBox.Text
+            End If
+
+            Form_Main.MyConnectionString = $"{My.Settings.NAA_DB_EXPConnectionString}User ID={UsernameTextBox.Text};Password={PasswordTextBox.Text}"
+            Form_Main.us = UsernameTextBox.Text
+            Dim sqlConnection1 As New SqlConnection(Form_Main.MyConnectionString)
+            sqlConnection1.Open()
+            sqlConnection1.Close()
+            Form_Main.Show()
+            Me.Close()
+        Catch sqlEx As SqlException
+            Debug.WriteLine(sqlEx.ToString)
+            MsgBox("Неправильное имя пользователя или пароль.", MsgBoxStyle.Critical)
+        Catch ex As Exception
+            MsgBox($"{ex.ToString}", MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form_Login_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If My.Settings.language = "Русский" Then
+            Me.Text = "Клиент базы данных эксперимента РЕГАТА - " & Application.ProductVersion
+            CheckBoxKeep.Text = "Запомнить"
+        ElseIf My.Settings.language = "English" Then
+            Me.Text = "Regata experiment data base client - " & Application.ProductVersion
+            CheckBoxKeep.Text = "Keep fields"
+        End If
+
+        If Not String.IsNullOrEmpty(My.Settings.user) And Not String.IsNullOrEmpty(My.Settings.password) Then
+            UsernameTextBox.Text = My.Settings.user
+            PasswordTextBox.Text = My.Settings.password
+            CheckBoxKeep.Checked = True
+            OK_Click(sender, e)
+        End If
+
+    End Sub
+End Class
+
